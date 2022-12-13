a0aa3c6004ecd102f9adf1e6f845e2df33ad23d0
kyuhyung <vision8love@gmail.com>
2022-12-13 16:48:17 +0900

    second commit

---
+++ services/sign.service.js
@@ -0,0 +1,25 @@
+const SignRepository = require("../repositories/sign.repository");
+const jwt = require("jsonwebtoken");
+
+class SignService {
+  signRepository = new SignRepository();
+
+  createUser = async (nickname, password) => {
+    const createUserData = await this.signRepository.createUser(nickname, password);
+
+    return createUserData;
+  };
+
+  login = async (nickname, password) => {
+    const user = await this.signRepository.findUser(nickname);
+    const token = await jwt.sign({ userId: user.userId, nickname: user.nickname }, "secret-key");
+
+    if (!user || password !== user.password) {
+      return "닉네임 또는 패스워드가 틀렸습니다.";
+    }
+
+    return token;
+  };
+}
+
+module.exports = SignService;
